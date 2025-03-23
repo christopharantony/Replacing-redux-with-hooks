de4952137470b90578a41e40928884d756771d38
Christophar Antony <christopharantony@gmail.com>
2025-03-24 01:50:51 +0530

    refactor: standardize quotation marks and improve code formatting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from "redux";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import productReducer from "./store/reducers/products";
 
 const rootReducer = combineReducers({
   shop: productReducer,
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
